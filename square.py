4934561f81c44c83339aa50097266d8aab34d89d
Kritika Maurya <kritikamaurya13@gmail.com>
2025-10-25 21:39:02 -0700

    Add square root calculation methods in square.py
    Implement two methods to calculate square roots.

---
+++ square.py
@@ -0,0 +1,12 @@
+# Method 1: Using exponentiation operator (**)
+num = float(input("Enter a number: "))
+sqrt = num ** 0.5
+print("The square root of", num, "is", sqrt)
+
+
+# Method 2: Using math.sqrt()
+import math
+
+num = float(input("Enter another number: "))
+sqrt = math.sqrt(num)
+print("The square root of", num, "is", sqrt)
